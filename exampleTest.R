2e224ce5311907c2b121ceee83fa0694cb7d7a18
Silvia-Pand <64426454+Silvia-Pand@users.noreply.github.com>
2022-04-10 08:25:14 +0200

    Add files via upload

--- exampleTest.R
+++ exampleTest.R
@@ -18,12 +18,12 @@ test1 <-HM_FM_Test(data=Y,id = "id",time="time",lv=2:3,responses=NULL,type=c("si
 
 # ---- display output ----
 print("p-value")
-print(test1$pvalueS)
+print(test1$pvalue)
 
 #Perform the test based on consecutive triplets of observations for l=4 
 test2 <-HM_FM_Test(data=Y,id = "id",time="time",lv=4,responses=NULL,type=c("triplets"))
 
 # ---- display output ----
 print("p-value")
-print(test2$pvalueT)
+print(test2$pvalue)
 
